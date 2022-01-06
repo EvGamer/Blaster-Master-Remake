f1807b5afdf078609cf21961be195b4a2dddcf1d
EvGamer <1evgenbetake@list.ru>
2022-01-06 17:03:06 +0300

    Add texture fragment

---
+++ src/graphics/TextureFragment.cpp
@@ -0,0 +1,23 @@
+#include "TextureFragment.h"
+
+TextureFragment::TextureFragment(TextureKeeper texture, Rectangle source) {
+  _texture = texture;
+  _getTextureCoordFromImageCoord(source);
+}
+
+TextureFragment::TextureFragment(const char* filename, Rectangle source) {
+  _texture = TextureKeeper(filename);
+  _getTextureCoordFromImageCoord(source);
+}
+
+void TextureFragment::_getTextureCoordFromImageCoord(Rectangle source) {
+  auto texture = _texture.getTexture();
+  _top = source.y / texture.height;
+  _bottom = (source.y + source.height) / texture.height;
+  _left = source.x / texture.width;
+  _right = (source.x + source.width) / texture.width;
+}
+
+void TextureFragment::draw(PixelPoint position) {
+
+}
---
+++ src/graphics/TextureFragment.h
@@ -0,0 +1,16 @@
+#include "TextureKeeper.h"
+
+class TextureFragment {
+  public:
+    TextureFragment(TextureKeeper texture, Rectangle source);
+    TextureFragment(const char* filename, Rectangle source);
+    void draw(Vector2 position);
+    void draw(Rectangle destination);
+  private:
+    void _getTextureCoordFromImageCoord(Rectangle source);
+    TextureKeeper _texture;
+    float _top;
+    float _bottom;
+    float _left;
+    float _right;
+}
