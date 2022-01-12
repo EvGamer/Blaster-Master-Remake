4207347d50e8e5231442b41271d2018e63eb7be2
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2022-01-12 18:08:49 +0300

    Make copy method private. Optimize constructors

--- src/graphics/TextureKeeper.cpp
+++ src/graphics/TextureKeeper.cpp
@@ -1,39 +1,29 @@
 #include "./TextureKeeper.h"
 
-TextureKeeper::TextureKeeper() {
-  _sharedCounter = new UInt(1);
-}
+TextureKeeper::TextureKeeper(): _sharedCounter{new UInt(1)} {}
 
-TextureKeeper::TextureKeeper(const char* filename) {
-  _sharedCounter = new UInt(1);
-  load(String(filename));
-}
+TextureKeeper::TextureKeeper(const char* filename): 
+  _sharedCounter{new UInt(1)},
+  _texture{LoadTexture(filename)},
+  _filename{filename}
+{}
 
-TextureKeeper::TextureKeeper(String filename) {
-  _sharedCounter = new UInt(1);
-  load(filename);
-}
+TextureKeeper::TextureKeeper(String filename): TextureKeeper(filename.c_str()) {}
 
-void TextureKeeper::load(String filename) {
-  _filename = filename;
-  _texture = LoadTexture(_filename.c_str());
-}
-
-void TextureKeeper::copy(const TextureKeeper& toCopy) {
+void TextureKeeper::_copy(const TextureKeeper& toCopy) {
   _texture = toCopy._texture;
   _filename = toCopy._filename;
   _sharedCounter = toCopy._sharedCounter;
-  _copyOrder = toCopy._copyOrder + 1;
   (*_sharedCounter)++;
   // TraceLog(LOG_INFO, "TEXKEEP: [ID: %i] Copy texture keeper. Counter value: %i", _texture.id, (*_sharedCounter));
 }
 
 TextureKeeper::TextureKeeper(const TextureKeeper& toCopy) {
-  copy(toCopy);
+  _copy(toCopy);
 }
 
 void TextureKeeper::operator=(const TextureKeeper& toCopy) {
-  copy(toCopy);
+  _copy(toCopy);
 }
 
 TextureKeeper::~TextureKeeper() {
@@ -44,7 +34,6 @@ TextureKeeper::~TextureKeeper() {
     // TraceLog(LOG_INFO, "TEXKEEP: [ID: %i] Decrement counter, Copy order: %i, Counter: %i, Filename: %s", textureId, _copyOrder, sharedCounter, _filename.c_str());
     return;
   }
-  TraceLog(LOG_INFO, "TEXKEEP: [ID: %i] Delete keeper. Copy Order: %i, Counter: %i, Filename: %s", textureId, _copyOrder, sharedCounter, _filename.c_str());
   UnloadTexture(_texture);
   delete _sharedCounter;
 }
