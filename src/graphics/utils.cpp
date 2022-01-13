a0ec900ac6341e7c0beec908e3a796fd6f8146e5
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2022-01-13 18:38:58 +0300

    Use old style gl rendering to reduce abstraction overhead

--- src/graphics/utils.cpp
+++ src/graphics/utils.cpp
@@ -2,31 +2,41 @@
 #include "../typeAliases.h"
 #include "../stdafx.h"
 #include "./TextureKeeper.h"
+#include "rlgl.h"
 
-const unsigned COORD_UNIT = 32;
+namespace {
+  const unsigned COORD_UNIT = 32;
+  const uint8_t MAX_COLOR = 255;
+}
 
 void drawSprite(
   const TextureKeeper& textureKeeper, float worldX0, float worldY0, float worldX, float worldY,
   float relativeCropX0, float relativeCropY0, float relativeCropX, float relativeCropY
 ) {
   Texture2D texture = textureKeeper.texture();
-  const float cropX0 = relativeCropX0 * texture.width;
-  const float cropX = relativeCropX * texture.width;
-  const float cropY0 = relativeCropY0 * texture.height;
-  const float cropY = relativeCropY * texture.height;
   const float x0 = worldX0 * COORD_UNIT;
   const float x = worldX * COORD_UNIT;
-  const float y0 = worldY0 * COORD_UNIT;
-  const float y = worldY * COORD_UNIT;
-  
-  const float cropWidth = cropX - cropX0;
-  const float cropHeight = cropY - cropY0;
-  const float width = x - x0;
-  const float height = y - y0;
-  Rectangle source{cropWidth > 0 ? cropX0 : cropX, cropY0, cropWidth, cropHeight};
-  Rectangle destination{x0, -y, width, height};
-  DrawTexturePro(
-    texture, source, destination,
-    {0, 0}, 0, WHITE
-  );
+  const float y = -worldY0 * COORD_UNIT;
+  const float y0 = -worldY * COORD_UNIT;
+
+  rlCheckRenderBatchLimit(4); // MAke sure there is enough free space for the batch buffer
+
+  rlSetTexture(texture.id);
+  rlBegin(RL_QUADS);
+    rlColor4ub(MAX_COLOR, MAX_COLOR, MAX_COLOR, MAX_COLOR);
+    rlNormal3f(0.0f, 0.0f, 1.0f); // Normal vector pointing towards viewer
+
+    rlTexCoord2f(relativeCropX0, relativeCropY0);
+    rlVertex2f(x0, y0);
+    
+    rlTexCoord2f(relativeCropX0, relativeCropY);
+    rlVertex2f(x0, y);
+
+    rlTexCoord2f(relativeCropX, relativeCropY);
+    rlVertex2f(x, y);
+
+    rlTexCoord2f(relativeCropX, relativeCropY0);
+    rlVertex2f(x, y0);
+  rlEnd();
+  rlSetTexture(0);
 }
