aa33f39a4b2adb361c8742776c41fbf39c7aa46b
Eugene-Kuzmenko <e.kuzmenko@dunice.net>
2022-01-13 17:57:12 +0300

    Cleanup animation class

--- src/graphics/Animation.h
+++ src/graphics/Animation.h
@@ -1,11 +1,11 @@
 #include "utils.h"
 #pragma once
 
-enum playType { ONCE = 0x00, LOOP = 0x01, REVERSE = 0x02 };
+enum PlayType { ONCE = 0x00, LOOP = 0x01, REVERSE = 0x02 };
 
 class Animation {
  private:
-  playType _play;
+  PlayType _play;
   TextureKeeper _texture;
   unsigned _timer;
   unsigned _delay;
@@ -24,21 +24,28 @@ class Animation {
 
   Animation();
 
-  Animation(TextureKeeper iTexture, float iSizeX, float iSizeY, uint8_t iX0,
-            uint8_t iY0, uint8_t iRows, uint8_t iColums, unsigned iDelay,
-            playType iPlay);
+  Animation(
+    TextureKeeper iTexture, float iSizeX, float iSizeY, 
+    uint8_t iX0, uint8_t iY0,
+    uint8_t iRows, uint8_t iColums, 
+    uint16_t iDelay, PlayType iPlay
+  );
 
-  Animation(TextureKeeper iTexture, float iSize, uint8_t iX0, uint8_t iY0,
-            uint8_t iRows, uint8_t iColums, unsigned iDelay, playType iPlay);
+  Animation(
+    TextureKeeper iTexture, float iSize, uint8_t iX0, uint8_t iY0,
+    uint8_t iRows, uint8_t iColums, unsigned iDelay, PlayType iPlay
+  );
 
-  inline void initialize(TextureKeeper iTexture, float iSizeX, float iSizeY,
-                         uint8_t iX0, uint8_t iY0, uint8_t iRows,
-                         uint8_t iColums, unsigned iDelay, playType iPlay);
+  inline void initialize(
+    TextureKeeper iTexture, float iSizeX, float iSizeY,
+    uint8_t iX0, uint8_t iY0, uint8_t iRows,
+    uint8_t iColums, unsigned iDelay, PlayType iPlay
+  );
 
   unsigned draw(char dir, float x1, float y1, float x2, float y2);
   inline void setCol(unsigned col) { _x = col; }
   inline void setRow(unsigned rou) { _y = rou; }
-  inline void freeze() { _stop = true; }
-  inline void unfreeze() { _stop = false; }
-  inline bool isEnded() { return _stop; }
+  inline void stop() { _stop = true; }
+  inline void resume() { _stop = false; }
+  inline const bool isStopped() const { return _stop; }
 };
